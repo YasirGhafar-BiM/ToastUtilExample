8e69456bce4d72a64a657a6c59637c099ea525be
YasirGhafar-BiM <yasir.ghafar@bigimmersive.com>
2021-06-22 19:56:47 +0500

    changes to test git push command

--- app/src/main/java/com/techlads/toastutilexample/MainActivity.kt
+++ app/src/main/java/com/techlads/toastutilexample/MainActivity.kt
@@ -2,10 +2,14 @@ package com.techlads.toastutilexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.techlads.toasterlib.ToastMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ToastMessage.s(this, "Used Example Lib to show this Toast")
+
     }
 }
